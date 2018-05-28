7e54ec6bf9fcda759c8955d196e230ec9a8ec487
lilac778 <thdals778@naver.com>
2018-05-29 03:50:22 +0900

    공지사항

--- routes/notice.js
+++ routes/notice.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var async = require('async');
 
 // MySQL 로드
 var mysql = require('mysql');
@@ -11,7 +14,74 @@ var pool = mysql.createPool({
 	password: '1234qwer'
 });
 
-
-router.get('/', function(req, res, next) {
-  res.render('notice', { title: 'notice' });
+router.get('/', function(req, res) {
+  var CurrPage  = Number(req.query.page);   // 현재 페이지 인덱스
+  if(!CurrPage)CurrPage = 1;
+	var TotalPage;  // 총 페이지 수
+	var articles = "";     // 게시판 내용
+  var pageArticleNum = 20; // 한 페이지에 표시될 게시글의 개수
+  var pageListNum = 10;  // 한 화면에 표시될 페이지 인덱스의 수
+  var startPage;  // 현재 화면 시작 인덱스
+  var endPage;    // 현재 화면 끝 인덱스
+  var Articles;
+	async.waterfall([
+	  function(callback){
+      pool.getConnection(function (err, connection) {
+        var sql = "SELECT COUNT(*) AS count FROM board1";
+        connection.query(sql, function(err, result){
+          if(err) console.error(err);
+          TotalPage = Math.ceil(result[0].count / pageArticleNum);
+          callback(null, TotalPage);
+        });
+      });
+    },
+    function(totalpage, callback){
+       pool.getConnection(function (err, connection) {
+        var sql = "SELECT idx, title, date, creator FROM board1";
+        connection.query(sql, function(err, result){
+          if(err) console.error(err);
+          articles = result;
+          var temp = {
+            articles: articles,
+            total: totalpage
+          }
+          callback(null, temp);
+        });
+      });
+    },
+    function(data, callback){
+      // 현재 페이지의 페이지네이션 시작 번호
+      startPage = ((Math.ceil(CurrPage/pageArticleNum) - 1) * pageArticleNum) + 1;
+      // 현재 페이지의 페이지네이션 끝 번호
+      endPage = (startPage + pageArticleNum) - 1;
+        // 만약 현재 페이지네이션 끝 번호가 페이지네이션 전체 카운트보다 높을 경우
+        if(endPage > TotalPage){
+          endPage = TotalPage;
+        }
+        Articles = {
+          contents: data.articles,
+          Current: CurrPage,
+          Start: startPage,
+          End: endPage,
+          Total: data.total,
+          ListCount: pageListNum
+        };
+        console.log(Articles.contents);
+        callback(null, Articles);
+      }
+  ],function(err, Articles){
+      if (err) {
+        throw err;
+      } else {
+        res.render('notice',{
+          title: 'notice',
+          articles: Articles,
+					username:req.session.username
+        });
+        console.log(Articles.idx);
+      }
+    }
+  );
 });
+
+module.exports = router;
