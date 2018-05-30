59d1b9618341b9bf85e62870278b2422009881f9
pjh6818 <a6846818@naver.com>
2018-05-30 11:02:36 +0900

    회원정보

--- routes/user.js
+++ routes/user.js
@@ -13,7 +13,27 @@ var pool = mysql.createPool({
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('user', {username:req.session.username});
+  var sql="Select email, date, username, address, phone, gender from user where email=?";
+  console.log(req.session);
+	pool.getConnection(function (err, connection) {
+	  if (err) throw err;
+	  // Use the connection
+	  connection.query(sql, [req.session.email], function (err, rows) {
+		  if(err) console.error(err);
+
+		  console.log("rows : " + JSON.stringify(rows));
+		  
+		  res.render('user', {username:req.session.username, title: '회원정보 조회', rows: rows});
+		  connection.release();  
+	  });
+	});
 });
 
+router.get('/modify', function(req, res, next) {
+  res.render('modify', {username:req.session.username});
+});
+
+router.post('/modify', function(req, res, next) {
+
+});
 module.exports = router;
