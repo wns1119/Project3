9951fcb1c19e2396e1397c8ad129f74e8c9238dc
pjh6818 <a6846818@naver.com>
2018-06-03 00:46:44 +0900

    판매자 권한 요청 추가

--- routes/user.js
+++ routes/user.js
@@ -133,4 +133,86 @@ router.post('/modify/3', function(req, res, next) {
 	});
 	res.redirect('/user');
 });
+
+router.get('/saleauthreq', function(req, res, next) {
+	var sql="Select * from user where email=?"
+
+	pool.getConnection(function (err, connection) {
+		if (err) throw err;
+	  // Use the connection
+	  connection.query(sql, [req.session.email], function (err, rows) {
+	  	if(err) console.error(err);
+
+	  	res.render('saleauthreq', {username:req.session.username, title: '판매자등록요청', rows: rows});
+	  	connection.release();
+	  	
+	  });
+	});
+});
+
+router.post('/saleauthreq', function(req, res, next) {
+	var sql="insert into SaleAuthReq(email, company, address, category, manager, phone1, phone2, content) Values(?,?,?,?,?,?,?,?)";
+	var email=req.session.email;
+	var company=req.body.company;
+	var address=req.body.address1+" "+req.body.address2;
+	var category=req.body.category;
+	var manager=req.body.manager;
+	var phone1=req.body.phone1;
+	var phone2=req.body.phone2;
+	var content=req.body.contents;
+	var data=[email, company, address, category, manager, phone1, phone2, content];
+
+	pool.getConnection(function (err, connection) {
+		if (err) throw err;
+	  // Use the connection
+	  connection.query(sql, data, function (err, rows) {
+	  	if(err) console.error(err);
+
+	  	console.log("rows : " + JSON.stringify(rows));
+
+	  	connection.release();
+	  });
+	});
+	res.redirect('/user');
+});
+
+router.get('/saleauthdel', function(req, res, next) {
+	var sql="Select * from SaleAuthReq where email=?"
+
+	pool.getConnection(function (err, connection) {
+		if (err) throw err;
+	  // Use the connection
+	  connection.query(sql, [req.session.email], function (err, rows) {
+	  	if(err) console.error(err);
+
+	  	res.render('saleauthdel', {username:req.session.username, title: '판매자등록삭제', rows: rows});
+	  	connection.release();
+	  	
+	  });
+	});
+});
+
+router.post('/saleauthdel', function(req, res, next) {
+	var sql="update user set sale=? where email=?";
+
+	pool.getConnection(function (err, connection) {
+		if (err) throw err;
+
+		connection.query(sql,[0, req.session.email], function (err, rows) {
+			if(err) console.error(err);
+
+			connection.release();
+		});
+	});
+	pool.getConnection(function (err, connection) {
+		if (err) throw err;
+		sql="delete from SaleAuthReq where email=?"
+		connection.query(sql,[req.session.email], function (err, rows) {
+			if(err) console.error(err);
+
+			connection.release();
+		});
+	});
+	res.redirect('/user');
+});
 module.exports = router;
