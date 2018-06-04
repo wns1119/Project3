8e22e6eba1c7e43af089cc48c70b88894b34c0a7
pjh6818 <a6846818@naver.com>
2018-06-04 15:36:32 +0900

    주문정보조회

--- routes/user.js
+++ routes/user.js
@@ -219,80 +219,17 @@ router.post('/saleauthdel', function(req, res, next) {
 });
 
 router.get('/orderinquiry', function(req, res, next) {
-	var CurrPage  = Number(req.query.page);   // 현재 페이지 인덱스
-	if(!CurrPage)CurrPage = 1;
-	var TotalPage;  // 총 페이지 수
-	var articles = "";     // 게시판 내용
-  	var pageArticleNum = 10; // 한 페이지에 표시될 게시글의 개수
-  	var pageListNum = 5;  // 한 화면에 표시될 페이지 인덱스의 수
-  	var startPage;  // 현재 화면 시작 인덱스
-  	var endPage;    // 현재 화면 끝 인덱스
-  	var Articles;
-  	async.waterfall([
-  		function(callback){
-
-  			pool.getConnection(function (err, connection) {
-  				var sql = "SELECT COUNT(*) AS count FROM order_ where email=?";
-  				connection.query(sql, function(err, result){
-  					if(err) console.error(err);
-  					TotalPage = Math.ceil(result[0].count / pageArticleNum);
-  					if(CurrPage>TotalPage)CurrPage=TotalPage;
-  					connection.release();
-  					totalpage = {
-  						total:TotalPage,
-  						Curr:CurrPage
-  					}
-  					callback(null, totalpage);
-  				});
-  			});
-  		},
-  		function(totalpage, callback){
-  			pool.getConnection(function (err, connection) {
-  				var sql = "SELECT idx, title, date_format(date, '%m-%d') as date, hit FROM board1 ORDER BY idx desc LIMIT ?, ?";
-  				connection.query(sql, [(totalpage.Curr-1)*pageArticleNum, pageArticleNum], function(err, result){
-  					if(err) console.error(err);
-  					articles = result;
-  					var temp = {
-  						articles: articles,
-  						total: totalpage.total,
-  						Curr: totalpage.Curr
-  					}
-  					connection.release();
-  					callback(null, temp);
-  				});
-  			});
-  		},
-  		function(data, callback){
-      // 현재 페이지의 페이지네이션 시작 번호
-      startPage = ((Math.ceil(data.Curr/pageListNum)-1) * pageListNum) + 1;
-      // 현재 페이지의 페이지네이션 끝 번호
-      endPage = (startPage + pageListNum) - 1;
-        // 만약 현재 페이지네이션 끝 번호가 페이지네이션 전체 카운트보다 높을 경우
-        if(endPage > TotalPage){
-        	endPage = TotalPage;
-        }
-        Articles = {
-        	contents: data.articles,
-        	Current: data.Curr,
-        	Start: startPage,
-        	End: endPage,
-        	Total: data.total,
-        	ListCount: pageListNum
-        };
-        callback(null, Articles);
-    }
-    ],function(err, Articles){
-    	if (err) {
-    		throw err;
-    	} else {
-    		res.render('notice',{
-    			title: 'notice',
-    			articles: Articles,
-    			username:req.session.username, 
-    			admin:req.session.admin
-    		});
-    	}
-    }
-    );
-  });
+	var sql = "SELECT product.code, order_.purchaser, product.name as name, order_.price, order_.amount, order_.address FROM product, order_ where order_.email=? and product.code=order_.product_code";
+	pool.getConnection(function (err, connection) {
+		if (err) throw err;
+	  // Use the connection
+	  connection.query(sql, [req.session.email], function (err, rows) {
+	  	if(err) console.error(err);
+	  	console.log(rows);
+	  	res.render('orderinquiry', {username:req.session.username, title: '판매자등록삭제', rows: rows, admin:req.session.admin});
+	  	connection.release();
+	  	
+	  });
+	});
+});
 module.exports = router;
